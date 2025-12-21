a9c8503e6d4a94d3c516840e90ab4ede05c9c423
dianedoan <Dianedd419@hotmail.com>
2025-12-20 18:01:04 -0700

    Remove getHumanChoice and playGame

--- script.js
+++ script.js
@@ -21,17 +21,6 @@ function getComputerChoice() {
 }
 
 /**
- * getHumanChoice
- * return one of the valid choices depending on what the user inputs
- */
-
-function getHumanChoice() {
-    // prompt user for choice
-    let humanChoice = prompt("Please enter a choice of rock, paper or scissors.");
-    return humanChoice;
-}
-
-/**
  * playRound
  * takes human and computer player choices as arguments, plays a single round, 
  * increments the round winner's score and logs a winner announcement
@@ -60,41 +49,6 @@ function playRound(humanChoice, computerChoice) {
     }
     return result;
 }
-
-/**
- * playGame
- * calls playRound to play 5 rounds 
- * keeps track of scores and declares a winner at the end
- */
-
-function playGame() {
-    // initialize score variables
-    let humanScore = 0;
-    let computerScore = 0;
-    
-    const humanSelection = getHumanChoice();
-    const computerSelection = getComputerChoice();
-    
-    const result = playRound(humanSelection, computerSelection);
-
-    // increment scores based on round winner
-    if (result == "humanWin") {
-        humanScore++;
-    } else if (result == "computerWin") {
-        computerScore++;
-    }
-
-    // log message for final winner after 5 rounds
-    if (humanScore > computerScore) {
-        console.log(`You win with a score of ${humanScore} : ${computerScore} after 5 rounds!`);
-    } else if (humanScore < computerScore) {
-        console.log(`You lose with a score of ${humanScore} : ${computerScore} after 5 rounds!`);
-    } else {
-        console.log(`It's a tie with a score of ${humanScore} : ${computerScore} after 5 rounds!`);
-    }
-}
-
-playGame();
 
 // add event listeners for buttons
 const buttons = document.querySelectorAll("button");
